26c6f9d84b2737a105f81e04be0d16cc9f63c275
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-04-23 17:47:52 -0400

    update

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 from flask import Flask, render_template
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-db = sqlite3.connect("post-collection.db")
-cursor = db.cursor()
 
-cursor.execute("CREATE TABLE posts (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL, content varchar(250) NOT NULL)")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///post-collection.db'
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(250), nullable=False)
+
+    
+    def __str__(self):
+       return f'<Post {self.title}>'
+    
+
+
+
+new_post = Post(id=1, title="First Post", content="Just my first post thats all")
+db.session.add(new_post)
+db.session.commit()
+
+db.create_all()
+
 
 
 @app.route("/")
