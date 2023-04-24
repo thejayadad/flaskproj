8a3c3143527c2efc1a166d4b4eee6599687c769a
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-04-23 23:44:33 -0400

    register and login

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-from flask import Flask,render_template,request,redirect,url_for,flash
+from flask import Flask,render_template,request,redirect,url_for,flash,session
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
+import werkzeug
+from flask_bcrypt import Bcrypt, check_password_hash
+
 
 app = Flask(__name__)
 app.secret_key='admin123'
+bcrypt = Bcrypt(app)
 
 
 db = sqlite3.connect("posts-collection.db")
+ub = sqlite3.connect("users-collection.db")
 cursor = db.cursor()
-# # cursor.execute("CREATE TABLE posts (id INTEGER PRIMARY KEY, title varchar(150) NOT NULL, content varchar(250) NOT NULL)")
+cur = ub.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, email varchar(150) NOT NULL, name varchar(250) NOT NULL, password varchar(250) NOT NULL)")
 
 # cursor.execute("INSERT INTO posts VALUES(2, 'Second Post', 'My second post my friend!')")
-# db.commit()
-
+db.commit()
+ub.commit()
 
 
 
@@ -68,7 +74,49 @@ def delete(id):
      flash('Post Deleted Successfully')
      return redirect(url_for("index"))
     
-   
+@app.route('/register',methods=['POST','GET'])
+def register():
+    if request.method=='POST':                
+            email = request.form['email']
+            name = request.form['name']          
+            password=request.form['password']
+            pw = bcrypt.generate_password_hash(password)
+            ub = sqlite3.connect("users-collection.db")
+            cur = ub.cursor()
+            cur.execute("insert into users(EMAIL,NAME,PASSWORD) values (?,?,?)",(email,name,pw))
+            ub.commit()
+            return redirect(url_for("index"))
+    return render_template("register.html")
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    if request.method=='POST':
+        email = request.form['email']
+        password = request.form['password']
+        ub = sqlite3.connect("users-collection.db")
+        cur = ub.cursor()
+        pw = bcrypt.generate_password_hash(password)
+        bcrypt.check_password_hash(pw, password)
+        cur.execute("select email,password FROM users where email = '+email+' and password='+pw+'")
+        ub.commit()
+        return redirect(url_for("index"))
+    return render_template("login.html")
+
+
+# @app.route("/add", methods=['POST','GET'])
+# def add():
+#    if request.method=='POST':
+#       title = request.form['title']
+#       content = request.form['content']
+#       db = sqlite3.connect("posts-collection.db")
+#       cursor = db.cursor()
+#       cursor.execute("insert into posts(TITLE,CONTENT) values (?,?)",(title,content))
+#       db.commit()
+#       return redirect(url_for("index"))
+#    return render_template("add.html")
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
